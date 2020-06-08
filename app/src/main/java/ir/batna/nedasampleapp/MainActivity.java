8d850934a2f22a623ec49b0a5159476a041beb09
ripek07 <iriik@yahoo.com>
2020-06-08 11:36:56 +0430

    test app v-1.2

--- app/src/main/java/ir/batna/nedasampleapp/MainActivity.java
+++ app/src/main/java/ir/batna/nedasampleapp/MainActivity.java
@@ -3,21 +3,25 @@ package ir.batna.nedasampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 import ir.batna.nedasdk.Neda;
 import ir.batna.nedasdk.OnClientRegisteredListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
         Neda.register(this, new OnClientRegisteredListener() {
             @Override
             public void onClientRegistered(String token) {
 
+                textView.setText("Your token is: " + token);
                 // do what u want here
             }
         });
