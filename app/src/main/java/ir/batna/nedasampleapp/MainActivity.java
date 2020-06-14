e850347d251092b2e8a348f46d2a2f3da2462c23
ripek07 <iriik@yahoo.com>
2020-06-14 16:50:49 +0430

    test app v-1.3

--- app/src/main/java/ir/batna/nedasampleapp/MainActivity.java
+++ app/src/main/java/ir/batna/nedasampleapp/MainActivity.java
@@ -2,8 +2,14 @@ package ir.batna.nedasampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ir.batna.nedasdk.Neda;
 import ir.batna.nedasdk.OnClientRegisteredListener;
@@ -11,17 +17,33 @@ import ir.batna.nedasdk.OnClientRegisteredListener;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView copy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
+        copy = findViewById(R.id.copy);
         Neda.register(this, new OnClientRegisteredListener() {
             @Override
-            public void onClientRegistered(String token) {
+            public void onClientRegistered(final String token) {
 
-                textView.setText("Your token is: " + token);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("Your token is: " + token);
+                        copy.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                ClipData clip = ClipData.newPlainText("token", token);
+                                clipboard.setPrimaryClip(clip);
+                                Toast.makeText(getApplicationContext(), "Token copied!", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                });
                 // do what u want here
             }
         });
