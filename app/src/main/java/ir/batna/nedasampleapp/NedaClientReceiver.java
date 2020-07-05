8b07b3ac668288f821b97afd4d06deee44d170a6
ripek07 <iriik@yahoo.com>
2020-07-05 17:04:31 +0430

    Migrated to new SDK

---
+++ app/src/main/java/ir/batna/nedasampleapp/NedaClientReceiver.java
@@ -0,0 +1,6 @@
+package ir.batna.nedasampleapp;
+
+import ir.batna.nedasdk.ClientReceiver;
+
+public class NedaClientReceiver extends ClientReceiver {
+}
--- app/src/main/java/ir/batna/nedasampleapp/NedaClientService.java
+++
@@ -1,7 +0,0 @@
-package ir.batna.nedasampleapp;
-
-import ir.batna.nedasdk.ClientService;
-
-public class NedaClientService extends ClientService {
-
-}
